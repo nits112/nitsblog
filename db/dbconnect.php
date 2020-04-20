7ff40812b737a6d7db59682dc1dae86712f62ea7
nits112 <112.nits@gmail.com>
2020-04-20 18:18:22 +0100

    included db connect in php

--- db/dbconnect.php
+++ db/dbconnect.php
@@ -1,7 +1,7 @@
 <?php
 
 /* Connection to database */
-	$conn =mysqli_connect("b79cec36b77084","19516659","","blog");
+	$conn =mysqli_connect("b79cec36b77084", "19516659", "eu-cdbr-west-03.cleardb.net", "heroku_16af6333e43605f");
 
 	/* Check connection */
 	if(mysqli_connect_error()) {
