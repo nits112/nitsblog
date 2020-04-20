77e7488351b00593c22db24e9b610459cb479fc1
nits112 <112.nits@gmail.com>
2020-04-20 22:34:05 +0100

    recaptcha

--- include/loginform.php
+++ include/loginform.php
@@ -8,15 +8,13 @@ session_start(); ?>
         <div class="form-group">
           	<input type="password" class="form-control" placeholder="Password" name="password">
         </div>
+		&nbsp;
+		<div class="g-recaptcha" data-sitekey="6LcUBOwUAAAAAInjkCnz8JRZpuwuJ4zul1_ppnNs"></div>
+		
         <button type="submit" class="btn btn-default" name="submit">Sign In</button>
 
         <a href=<?php echo $register_url; ?> class="btn btn-default">Sign Up</a>
 		
-		<div class="form-group">
-              <input type="text"   name="verficationcode" maxlength="5" autocomplete="off" required  style="width: 200px;"  placeholder="Enter Captcha" autofocus />&nbsp;
-              <!--Cpatcha Image -->
-              <img src="security_image.php" alt="security code" />
-              </div>
  </form>
  <?php  unset($_SESSION['captcha']); // To reset the captcha ?>
 
--- users/login.php
+++ users/login.php
@@ -7,10 +7,35 @@ include("../db/dbconnect.php");
 include("../include/url_users.php");
 
 if(isset($_POST['submit'])) {
-if ($_POST["verficationcode"] != $_SESSION["captcha"] OR $_SESSION["captcha"]=='')
-    {
-    echo "<script>alert('Incorrect captcha');</script>" ;
-    }
+$response = $_POST["g-recaptcha-response"];
+
+	$url = 'https://www.google.com/recaptcha/api/siteverify';
+	$data = array(
+		'secret' => 'YOUR_SECRET',
+		'response' => $_POST["g-recaptcha-response"]
+	);
+	$options = array(
+		'http' => array (
+			'method' => 'POST',
+			'content' => http_build_query($data)
+		)
+	);
+	$context  = stream_context_create($options);
+	$verify = file_get_contents($url, false, $context);
+	$captcha_success=json_decode($verify);
+
+	if ($captcha_success->success==false) {
+		echo "
+		<div class=\"alert alert-danger container\" role=\"alert\">
+	  <span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span>
+	  <span class=\"sr-only\">Error:</span>
+	   You are a bot!! Go Away
+		</div>
+		";
+	} 
+	
+	
+	
 	$username=$_POST['username'];
 	$password=$_POST['password'];
 
