c2709ff9c1873b6c3409e0f387c24813f6c11c1c
nits112 <112.nits@gmail.com>
2020-04-22 20:39:36 +0100

    feedback validation

--- include/frame_contact_us.php
+++ include/frame_contact_us.php
@@ -17,10 +17,8 @@ if(isset($_POST['submit'])) {
         if (!$_POST['message']) {
         $errMessage = 'Please enter your message';
         }
-        //Check if simple anti-bot test is correct
-        if ($human !== 5) {
-        $errHuman = 'Your anti-spam is incorrect';
-        }
+      
+        
 } else {
   $errName="";
   $errEmail="";
@@ -35,31 +33,25 @@ if(isset($_POST['submit'])) {
       <div class="form-group">
           <label for="name" class="col-sm-3 control-label">Name</label>
           <div class="col-sm-6 ">
-              <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" >
+              <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" required>
               <?php echo "<p class='text-danger'>$errName</p>";?>
           </div>
       </div>
       <div class="form-group">
           <label for="email" class="col-sm-3 control-label">Email</label>
           <div class="col-sm-6">
-              <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com">
+              <input type="email" class="form-control" id="email" name="email" placeholder="example@domain.com" required>
               <?php echo "<p class='text-danger'>$errEmail</p>";?>
           </div>
       </div>
       <div class="form-group">
           <label for="message" class="col-sm-3 control-label">Message</label>
           <div class="col-sm-6">
-              <textarea class="form-control" rows="4" name="message"></textarea>
+              <textarea class="form-control" rows="4" name="message" required></textarea>
               <?php echo "<p class='text-danger'>$errMessage</p>";?>
           </div>
       </div>
-      <div class="form-group">
-          <label for="human" class="col-sm-3 control-label">2 + 3 = ?</label>
-          <div class="col-sm-6">
-              <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
-              <?php echo "<p class='text-danger'>$errHuman</p>";?>
-          </div>
-      </div>
+      
       <div class="form-group">
           <div class="col-sm-10 col-sm-offset-2">
               <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
