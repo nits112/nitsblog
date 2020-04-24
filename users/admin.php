b9d40c9840e5e249556fa92530b5f5ecccb401e7
nits112 <112.nits@gmail.com>
2020-04-24 16:38:41 +0100

    admin console fixing

--- users/admin.php
+++ users/admin.php
@@ -17,13 +17,15 @@ else {
 
 
 <body>
-
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
 <div class="container">
   <h2>Admin Panel</h2>
   <ul class="nav nav-tabs">
     <li class="active"><a data-toggle="tab" href="#posts">Post Requests</a></li>
     <li><a data-toggle="tab" href="#acc">Account Requests</a></li>
-    
+    <li><a data-toggle="tab" href="#user">User Details</a></li>
+		<li><a data-toggle="tab" href="#messages">Messages</a></li>
   </ul>
 
   <div class="tab-content">
@@ -43,7 +45,21 @@ else {
 			</p>
     </div>
 
-   
+    <div id="user" class="tab-pane fade">
+      <p>
+				<?php
+				   include("userlist.php");
+				?>
+			</p>
+    </div>
+
+		<div id="messages" class="tab-pane fade">
+			<p>
+				<?php
+					 include("../include/messages.php");
+				?>
+			</p>
+		</div>
 
   </div>
 </div>
